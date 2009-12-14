b5e80c55a20429c84d8b6f1bcd9a81e6eab46aa9
Federico Maggi <federico@maggi.cc>
2009-12-14 12:52:23 +0100

    Used find_packages from setuptools.

--- setup.py
+++ setup.py
@@ -1,11 +1,7 @@
 # this is all borrowed from Django source-code
 
 from distutils.core import setup
-from distutils.command.install_data import install_data
-from distutils.command.install import INSTALL_SCHEMES
-
-import os
-import sys
+from setuptools import find_packages
 
 try:
     from foopackage import get_version
@@ -13,47 +9,10 @@ try:
 except ImportError:
     VERSION = '0.1-alpha-1'
 
-class osx_install_data(install_data):
-    def finalize_options(self):
-        self.set_undefined_options('install', ('install_lib', 'install_dir'))
-        install_data.finalize_options(self)
-
-if sys.platform == "darwin": 
-    cmdclasses = {'install_data': osx_install_data} 
-else: 
-    cmdclasses = {'install_data': install_data} 
-
-def fullsplit(path, result=None):
-    """
-    Split a pathname into components (the opposite of os.path.join) in
-    a platform-neutral way.
-    """
-    if result is None:
-        result = []
-    head, tail = os.path.split(path)
-    if head == '':
-        return [tail] + result
-    if head == path:
-        return result
-    return fullsplit(head, [tail] + result)
-
-for scheme in INSTALL_SCHEMES.values():
-    scheme['data'] = scheme['purelib']
-
-packages, data_files = [], []
-root_dir = os.path.dirname(__file__)
-if root_dir != '':
-    os.chdir(root_dir)
-package_dir = 'foopackage'
-
-for dirpath, dirnames, filenames in os.walk(package_dir):
-    for i, dirname in enumerate(dirnames):
-        if dirname.startswith('.'): del dirnames[i]
-    if '__init__.py' in filenames:
-        packages.append('.'.join(fullsplit(dirpath)))
-    elif filenames:
-        data_files.append(
-            [dirpath, [os.path.join(dirpath, f) for f in filenames]])
+try:
+    long_description = open('README').read()
+except:
+    long_description = ''
 
 setup(
     name = 'django-foopackage',
@@ -62,20 +21,19 @@ setup(
     author = 'Federico Maggi',
     author_email = 'federico@maggi.cc',
     description = 'A Django package that provides Foo.',
+    long_description = long_description,
     
-    packages = packages,
-    cmdclass = cmdclasses,
-    data_files = data_files,
-
-        classifiers = [
+    packages = find_packages(),
+    
+    classifiers = [
         'Development Status :: 3 - Alpha',
         'Environment :: Web Environment',
-                'Framework :: Django',
+        'Framework :: Django',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: BSD License',
         'Operating System :: OS Independent',
         'Programming Language :: Python',
-                'Topic :: Software Development :: Libraries :: Python Modules',
-                'Topic :: Utilities'
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Topic :: Utilities'
         ]
 )
