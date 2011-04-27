382ef8f5cae8cab99dadccc76172143b11444a9f
phretor <none@none>
2011-04-27 23:47:50 +0200

    removed unnecessary lines

--- academic/content/urls.py
+++ academic/content/urls.py
@@ -1,7 +1,5 @@
 from django.conf.urls.defaults import *
 from django.conf import settings
-from django.views.decorators.cache import cache_page
-from django.views.generic.list_detail import object_list, object_detail
 
 from academic.content.models import *
 
